e34de0c8bf68f094ef63852542e31549ac2fd9ad
EricaZhu <dongjiezhu1994@hotmail.com>
2015-08-05 22:15:40 +0800

    code

---
+++ stylesheets/default.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+function show(id) {
+	document.getElementByld(id).style.display='block';
+}
+function hide(id) {
+	document.getElementByld(id).style.display='none';
+}
+</script>
